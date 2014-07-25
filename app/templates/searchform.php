3ce66678e0c3891eec65d654e7b99c29dcba97f0
Guilherme Marconi <guiilhermemarconi@gmail.com>
2014-07-25 16:38:03 -0300

    alterando tags PHP pelas solicitadas pelo debug do WP

--- app/templates/searchform.php
+++ app/templates/searchform.php
@@ -1,3 +1,3 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo( 'url' ) ?>/">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>/">
     <input type="search" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="Busca">
 </form>
