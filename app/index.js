77895b84c6834abcf24c09c3af8157544346ae55
Matheus Guilarducci <mguilarducci@gmail.com>
2014-10-27 19:46:14 -0200

    Refatorando com testes!

--- app/index.js
+++ app/index.js
@@ -1,93 +1,100 @@
 'use strict';
 
-var yeoman = require('yeoman-generator');
+var yeoman = require('yeoman-generator'),
+  path = require('path'),
+  util = require('util');
 
-var GeneratorWPGTX = yeoman.generators.Base.extend({
-  promptUser: function () {
-    var done = this.async();
+var GeneratorWPGTX = module.exports = function GeneratorWPGTX(args, options) {
+  yeoman.generators.Base.apply(this, arguments);
 
-    console.log(this.yeoman);
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
 
-    var prompts = [{
-      name: 'appName',
-      message: 'Qual o nome do tema do projeto?'
-    }];
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
 
-    this.prompt(prompts, function (props) {
-      this.appName = props.appName;
+util.inherits(GeneratorWPGTX, yeoman.generators.Base);
 
-      done();
-    }.bind(this));
-  },
+GeneratorWPGTX.prototype.askFor = function askFor() {
+  var done = this.async();
 
-  scaffoldFolders: function () {
-    var dir = this._.slugify(this.appName);
+  console.log(this.yeoman);
 
-    this.mkdir(dir);
-  },
+  var prompts = [{
+    name: 'appName',
+    message: 'Qual o nome do tema do projeto?'
+  }];
 
-  projectfiles: function () {
-    var index;
-    var name = this.appName;
-    var dir = this._.slugify(name);
-    var files = [
-      'img/sprite.png',
-      'inc/content-aside.php',
-      'inc/content-audio.php',
-      'inc/content-chat.php',
-      'inc/content-gallery.php',
-      'inc/content-image.php',
-      'inc/content-link.php',
-      'inc/content-quote.php',
-      'inc/content-video.php',
-      'inc/content.php',
-      'inc/loop-single.php',
-      'inc/loop.php',
-      'js/scripts.js',
-      'js/min/scripts.min.js',
-      'js/vendor/fitvids.min.js',
-      'js/vendor/flexslider.min.js',
-      'js/vendor/modernizr.min.js',
-      'scss/_base.scss',
-      'scss/_bootstrap.scss',
-      'scss/_flexslider.scss',
-      'scss/_normalize.scss',
-      '404.php',
-      'archive.php',
-      'author.php',
-      'category.php',
-      'config.rb',
-      'date.php',
-      'footer.php',
-      'functions.php',
-      'header.php',
-      'index.php',
-      'page.php',
-      'screenshot.png',
-      'search.php',
-      'searchform.php',
-      'sidebar.php',
-      'single.php',
-      'tag.php'
-    ];
+  this.prompt(prompts, function (props) {
+    this.appName = props.appName;
 
-    for (index = 0; index < files.length; ++index) {
-      this.copy(files[index], dir + '/' + files[index]);
-    }
+    done();
+  }.bind(this));
+};
 
-    this.copy('ftppass', dir + '/.ftppass');
-    this.copy('gitignore', dir + '/.gitignore');
+GeneratorWPGTX.prototype.app = function app() {
+  this.mkdir('img');
+  this.mkdir('inc');
+  this.mkdir('js');
+  this.mkdir('js/min');
+  this.mkdir('js/vendor');
+  this.mkdir('scss');
+};
 
-    var context = {
-      /*jshint camelcase: false */
-      theme_name: name
-    };
+GeneratorWPGTX.prototype.git = function git() {
+  this.copy('gitignore', '.gitignore');
+};
 
-    this.template('_gruntfile.js', dir + '/Gruntfile.js', context);
-    this.template('_package.json', dir + '/package.json', context);
-    this.template('_style.scss', dir + '/scss/style.scss', context);
-    this.template('_style.css', dir + '/style.css', context);
-  }
-});
+GeneratorWPGTX.prototype.img = function img() {
+  this.copy('img/sprite.png', 'img/sprite.png');
+};
 
-module.exports = GeneratorWPGTX;
+GeneratorWPGTX.prototype.inc = function inc() {
+  this.copy('inc/content-aside.php', 'inc/content-aside.php');
+  this.copy('inc/content-audio.php', 'inc/content-audio.php');
+  this.copy('inc/content-chat.php', 'inc/content-chat.php');
+  this.copy('inc/content-gallery.php', 'inc/content-gallery.php');
+  this.copy('inc/content-image.php', 'inc/content-image.php');
+  this.copy('inc/content-link.php', 'inc/content-link.php');
+  this.copy('inc/content-quote.php', 'inc/content-quote.php');
+  this.copy('inc/content-video.php', 'inc/content-video.php');
+  this.copy('inc/content.php', 'inc/content.php');
+  this.copy('inc/loop-single.php', 'inc/loop-single.php');
+  this.copy('inc/loop.php', 'inc/loop.php');
+};
+
+GeneratorWPGTX.prototype.js = function js() {
+  this.copy('js/scripts.js', 'js/scripts.js');
+  this.copy('js/min/scripts.min.js', 'js/min/scripts.min.js');
+  this.copy('js/vendor/fitvids.min.js', 'js/vendor/fitvids.min.js');
+  this.copy('js/vendor/flexslider.min.js', 'js/vendor/flexslider.min.js');
+  this.copy('js/vendor/modernizr.min.js', 'js/vendor/modernizr.min.js');
+};
+
+GeneratorWPGTX.prototype.scss = function scss() {
+  this.copy('scss/_base.scss', 'scss/_base.scss');
+  this.copy('scss/_bootstrap.scss', 'scss/_bootstrap.scss');
+  this.copy('scss/_flexslider.scss', 'scss/_flexslider.scss');
+  this.copy('scss/_normalize.scss', 'scss/_normalize.scss');
+};
+
+GeneratorWPGTX.prototype.files = function files() {
+  this.copy('404.php', '404.php');
+  this.copy('archive.php', 'archive.php');
+  this.copy('author.php', 'author.php');
+  this.copy('category.php', 'category.php');
+  this.copy('config.rb', 'config.rb');
+  this.copy('date.php', 'date.php');
+  this.copy('footer.php', 'footer.php');
+  this.copy('functions.php', 'functions.php');
+  this.copy('header.php', 'header.php');
+  this.copy('index.php', 'index.php');
+  this.copy('page.php', 'page.php');
+  this.copy('screenshot.png', 'screenshot.png');
+  this.copy('search.php', 'search.php');
+  this.copy('searchform.php', 'searchform.php');
+  this.copy('sidebar.php', 'sidebar.php');
+  this.copy('single.php', 'single.php');
+  this.copy('tag.php', 'tag.php');
+};
--- test/test-creation.js
+++ test/test-creation.js
@@ -1,7 +1,8 @@
-/*global describe, beforeEach, it */
+/*global describe, beforeEach, afterEach, it */
 'use strict';
-var path = require('path');
-var helpers = require('yeoman-generator').test;
+var path = require('path'),
+  helpers = require('yeoman-generator').test,
+  rimraf = require('rimraf');
 
 describe('wpgtx generator', function () {
   beforeEach(function (done) {
@@ -17,11 +18,62 @@ describe('wpgtx generator', function () {
     }.bind(this));
   });
 
+  afterEach(function(done) {
+    rimraf(path.join(__dirname, 'temp'), done);
+  });
+
   it('creates expected files', function (done) {
     var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
+      // git
+      '.gitignore',
+
+      // img
+      'img/sprite.png',
+
+      // inc
+      'inc/content-aside.php',
+      'inc/content-audio.php',
+      'inc/content-chat.php',
+      'inc/content-gallery.php',
+      'inc/content-image.php',
+      'inc/content-link.php',
+      'inc/content-quote.php',
+      'inc/content-video.php',
+      'inc/content.php',
+      'inc/loop-single.php',
+      'inc/loop.php',
+
+      // js
+      'js/scripts.js',
+      'js/min/scripts.min.js',
+      'js/vendor/fitvids.min.js',
+      'js/vendor/flexslider.min.js',
+      'js/vendor/modernizr.min.js',
+
+      // scss
+      'scss/_base.scss',
+      'scss/_bootstrap.scss',
+      'scss/_flexslider.scss',
+      'scss/_normalize.scss',
+
+      // files
+      '404.php',
+      'archive.php',
+      'author.php',
+      'category.php',
+      'config.rb',
+      'date.php',
+      'footer.php',
+      'functions.php',
+      'header.php',
+      'index.php',
+      'page.php',
+      'screenshot.png',
+      'search.php',
+      'searchform.php',
+      'sidebar.php',
+      'single.php',
+      'tag.php'
     ];
 
     helpers.mockPrompt(this.app, {
